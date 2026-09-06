ecd32f0eb8c29c28afdaa8abcfa01bd86b87f666
CCrisix <omgitzrllyu@gmail.com>
2026-09-06 18:18:02 -0400

    Correct FNV-1a naming

--- src/HashTable.c
+++ src/HashTable.c
@@ -2,15 +2,15 @@
 #include <stdint.h>
 #include <stdio.h>
 
-const static uint64_t FNV_OFFSET_BIAS = UINT64_C(14695981039346656037);
-const static uint64_t FNV_PRIME = UINT64_C(1099511628211);
+const static uint64_t FNV1a_OFFSET_BIAS = UINT64_C(14695981039346656037);
+const static uint64_t FNV1a_PRIME = UINT64_C(1099511628211);
 
-uint64_t hashFNV(const char *item){
-    uint64_t hash = FNV_OFFSET_BIAS;
+static uint64_t hashFNV1a(const char *item){
+    uint64_t hash = FNV1a_OFFSET_BIAS;
     for (size_t i = 0; item[i] != '\0'; i++) {
         uint8_t const cur_char = item[i];
         hash ^= cur_char;
-        hash *= FNV_PRIME;
+        hash *= FNV1a_PRIME;
     }
     return hash;
 }
