4b002eab6ca79daad0660fd2afcf2e8779c3c38d
CCrisix <omgitzrllyu@gmail.com>
2026-09-06 18:59:59 -0400

    Add hash table data structures

--- include/HashTable.h
+++ include/HashTable.h
@@ -2,4 +2,6 @@
 #define C_DATA_STRUCTURES_HASHTABLE_H
 #include <stdint.h>
 
+typedef struct HashTable HashTable;
+
 #endif //C_DATA_STRUCTURES_HASHTABLE_H
--- src/HashTable.c
+++ src/HashTable.c
@@ -2,11 +2,25 @@
 #include <stdint.h>
 #include <stdio.h>
 
-const static uint64_t FNV1a_OFFSET_BIAS = UINT64_C(14695981039346656037);
+
+typedef struct Entry {
+    char *key ;
+    void* value;
+    uint64_t cached_hash;
+    struct Entry *next;
+} Entry;
+
+struct HashTable{
+    Entry **buckets;
+    size_t num_buckets;
+    size_t size;
+};
+
+const static uint64_t FNV1a_OFFSET_BASIS = UINT64_C(14695981039346656037);
 const static uint64_t FNV1a_PRIME = UINT64_C(1099511628211);
 
 static uint64_t hashFNV1a(const char *item){
-    uint64_t hash = FNV1a_OFFSET_BIAS;
+    uint64_t hash = FNV1a_OFFSET_BASIS;
     for (size_t i = 0; item[i] != '\0'; i++) {
         uint8_t const cur_char = item[i];
         hash ^= cur_char;
