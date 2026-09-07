3695a59eb6e354f1616f4c4010dfb6dceedf92fa
CCrisix <omgitzrllyu@gmail.com>
2026-09-06 21:56:03 -0400

    Add hash table creation

--- include/HashTable.h
+++ include/HashTable.h
@@ -1,7 +1,16 @@
 #ifndef C_DATA_STRUCTURES_HASHTABLE_H
 #define C_DATA_STRUCTURES_HASHTABLE_H
-#include <stdint.h>
+#include<stddef.h>
+
+typedef enum {
+    HASH_TABLE_SUCCESS,
+    HASH_TABLE_INVALID_CAPACITY,
+    HASH_TABLE_ALLOCATION_FAILED,
+    HASH_TABLE_INVALID_OUTPUT,
+    HASH_TABLE_ALREADY_INITIALIZED
+}HashTableStatus;
 
 typedef struct HashTable HashTable;
+HashTableStatus createHashTable(size_t num_buckets, HashTable** table);
 
 #endif //C_DATA_STRUCTURES_HASHTABLE_H
--- src/HashTable.c
+++ src/HashTable.c
@@ -1,6 +1,6 @@
 #include "HashTable.h"
-#include <stdint.h>
-#include <stdio.h>
+#include<stdint.h>
+#include<stdlib.h>
 
 
 typedef struct Entry {
@@ -31,4 +31,34 @@ static uint64_t hashFNV1a(const char *item){
 
 static size_t hashToIndex(const uint64_t hash, const size_t numBuckets) {
     return hash % numBuckets;
+}
+
+HashTableStatus createHashTable(const size_t num_buckets, HashTable** table) {
+    if (num_buckets == 0) {
+        return HASH_TABLE_INVALID_CAPACITY;
+    }
+    if (table == NULL) {
+        return HASH_TABLE_INVALID_OUTPUT;
+    }
+    if (*table != NULL) {
+        return HASH_TABLE_ALREADY_INITIALIZED;
+    }
+
+    HashTable* hash_table = malloc(sizeof(HashTable));
+    if (hash_table == NULL) {
+        return HASH_TABLE_ALLOCATION_FAILED;
+    }
+
+    Entry** buckets = calloc(num_buckets, sizeof(Entry*));
+    if (buckets == NULL) {
+        free(hash_table);
+        return HASH_TABLE_ALLOCATION_FAILED;
+    }
+
+    hash_table->buckets = buckets;
+    hash_table->num_buckets = num_buckets;
+    hash_table->size = 0;
+    *table = hash_table;
+
+    return HASH_TABLE_SUCCESS;
 }
